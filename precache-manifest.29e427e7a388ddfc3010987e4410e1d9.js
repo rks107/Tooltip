056beffb958c735c5a2b5ebed462c092d07c5146
rohit kumar <rohit1071998@gmail.com>
2020-08-08 21:10:58 +0530

    Updates

---
+++ precache-manifest.29e427e7a388ddfc3010987e4410e1d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c6ffae11b7f95e6f69d0cf223cc5fb",
+    "url": "/Tooltip/index.html"
+  },
+  {
+    "revision": "ab7888b01f78a85eaf21",
+    "url": "/Tooltip/static/css/2.673b3409.chunk.css"
+  },
+  {
+    "revision": "0485600676be7f983338",
+    "url": "/Tooltip/static/css/main.8c14d97f.chunk.css"
+  },
+  {
+    "revision": "ab7888b01f78a85eaf21",
+    "url": "/Tooltip/static/js/2.d297557c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Tooltip/static/js/2.d297557c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0485600676be7f983338",
+    "url": "/Tooltip/static/js/main.fa57dc8f.chunk.js"
+  },
+  {
+    "revision": "0d5c63781616c6e8a024",
+    "url": "/Tooltip/static/js/runtime-main.33c194d2.js"
+  }
+]);
